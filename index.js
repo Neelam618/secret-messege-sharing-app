2beee9c25f5ea74a0068621495e6bc858a8acf63
Neelam Chavan <neelamchavan410@gmail.com>
2022-03-11 11:08:02 +0530

    encode the entered string

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault()      //to avoid brouser default behavior: sending form data to backend server and refreshing the page which we don't have here
-    const input = document.querySelector('input')
+    const input = document.querySelector('#msg-input')
+    const encrypted = btoa(input.value)
+    document.querySelector('#link-input').value = encrypted
 })
