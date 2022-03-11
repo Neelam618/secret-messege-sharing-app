dc31e3db3351223c09f6844a0b9e3fb15d1b13d8
Neelam Chavan <neelamchavan410@gmail.com>
2022-03-11 11:27:55 +0530

    generate url and select it

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 document.querySelector('form').addEventListener('submit', event => {
     event.preventDefault()      //to avoid brouser default behavior: sending form data to backend server and refreshing the page which we don't have here
-    const input = document.querySelector('#msg-input')
-    const encrypted = btoa(input.value)
-    document.querySelector('#link-input').value = encrypted
+    const msgInput = document.querySelector('#msg-input')
+    const encrypted = btoa(msgInput.value)
+
+    const linkInput = document.querySelector('#link-input')
+    linkInput.value = `${window, location}#${encrypted}`
+    linkInput.select()
 })
